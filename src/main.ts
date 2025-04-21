75cfba128c586808a17c0d5c4f2af6b34c155990
Keson.Cai <keson.cai@kerryprops.com>
2025-04-21 17:19:34 +0800

    111

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    // origin: '*', // 允许任何来源 - 开发时可以，生产环境建议指定明确来源
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // 允许的方法
+    credentials: true, // 允许携带凭证 (例如 cookies, Authorization header)
+  });
   await app.listen(3000);
 }
 bootstrap();
